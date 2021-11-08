1925fd3170f7afaff7299445e37eadc92664e621
zyfrontend <frontendzy@gmail.com>
2021-11-08 11:57:10 +0800

    bug修复

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from "react";
-import { renderRoutes } from "react-router-config";
-import { HashRouter as Router } from "react-router-dom";
+import React, { Suspense } from "react"
+import { renderRoutes } from "react-router-config"
+import { BrowserRouter as Router } from "react-router-dom"
 // redux
-import { Provider } from "react-redux";
-import store from "@/store";
+import { Provider } from "react-redux"
+import store from "@/store"
 // 路由配置
-import routes from "@/router";
+import routes from "@/router"
 // 导航栏
-import NavBar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import PlayerBar from "@/components/PlayerBar";
+import NavBar from "@/components/Navbar"
+import Footer from "@/components/Footer"
+import PlayerBar from "@/components/PlayerBar"
 export default function App() {
-	return (
-		<div className="container">
-			<Provider store={store}>
-				<Router>
-					<NavBar />
-					{renderRoutes(routes)}
-					<Footer />
-					<PlayerBar />
-				</Router>
-			</Provider>
-		</div>
-	);
+  return (
+    <div className="container">
+      <Provider store={store}>
+        <Router>
+          <NavBar />
+          <Suspense fallback={<div>loading</div>}>
+            {renderRoutes(routes)}
+          </Suspense>
+          <Footer />
+          <PlayerBar />
+        </Router>
+      </Provider>
+    </div>
+  )
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,63 @@
-import React from "react";
-import { Redirect } from "react-router-dom";
+import React from "react"
+import { Redirect } from "react-router-dom"
 
-// 组件
-import Discover from "@/pages/Discover";
-// 子页面
-import Recommend from "@/pages/Discover/pages/Recommend";
-import LeaderBoard from "@/pages/Discover/pages/LeaderBoard";
-import SongList from "@/pages/Discover/pages/SongList";
-import AnchorStation from "@/pages/Discover/pages/AnchorStation";
-import MusicList from "@/pages/Discover/pages/MusicList";
+// // 组件
+// import Discover from "@/pages/Discover"
+// // 子页面
+// import Recommend from "@/pages/Discover/pages/Recommend"
+// import LeaderBoard from "@/pages/Discover/pages/LeaderBoard"
+// import SongList from "@/pages/Discover/pages/SongList"
+// import AnchorStation from "@/pages/Discover/pages/AnchorStation"
+// import MusicList from "@/pages/Discover/pages/MusicList"
+
+const Discover = React.lazy(() => import("@/pages/Discover"))
+const Recommend = React.lazy(() => import("@/pages/Discover/pages/Recommend"))
+const LeaderBoard = React.lazy(() =>
+  import("@/pages/Discover/pages/LeaderBoard")
+)
+const SongList = React.lazy(() => import("@/pages/Discover/pages/SongList"))
+const AnchorStation = React.lazy(() =>
+  import("@/pages/Discover/pages/AnchorStation")
+)
+const MusicList = React.lazy(() => import("@/pages/Discover/pages/MusicList"))
+
 const routes = [
-	{
-		path: "/",
-		exact: true,
-		render: () => <Redirect to="/discover" />,
-	},
-	{
-		path: "/discover",
-		component: Discover,
-		routes: [
-			{
-				path: "/discover",
-				exact: true,
-				render: () => <Redirect to="/discover/recommend" />,
-			},
-			{
-				path: "/discover/recommend",
-				component: Recommend,
-			},
-			{
-				path: "/discover/ranking",
-				component: LeaderBoard,
-			},
-			{
-				path: "/discover/songs",
-				component: SongList,
-			},
-			{
-				path: "/discover/djradio",
-				component: AnchorStation
-			},
-			{
-				path: "/discover/musiclist/:id",
-				component: MusicList
-			}
-		],
-	},
-];
+  {
+    path: "/",
+    exact: true,
+    render: () => <Redirect to="/discover" />,
+  },
+  {
+    path: "/discover",
+    component: Discover,
+    routes: [
+      {
+        path: "/discover",
+        exact: true,
+        render: () => <Redirect to="/discover/recommend" />,
+      },
+      {
+        path: "/discover/recommend",
+        component: Recommend,
+      },
+      {
+        path: "/discover/ranking",
+        component: LeaderBoard,
+      },
+      {
+        path: "/discover/songs",
+        component: SongList,
+      },
+      {
+        path: "/discover/djradio",
+        component: AnchorStation,
+      },
+      {
+        path: "/discover/musiclist/:id",
+        component: MusicList,
+      },
+    ],
+  },
+]
 
-export default routes;
+export default routes
